7857767acf86fcd4013e50841242fc623f10cb50
Juan Ignacio Zotelo <80764320+Juanignacioz@users.noreply.github.com>
2025-03-19 23:31:45 -0400

    se intenta agregar fotos

--- src/App/Vistas/Perfil/Perfil.tsx
+++ src/App/Vistas/Perfil/Perfil.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { Tabs, Tab, Box, Container, Avatar } from '@mui/material'
+import { Tabs, Tab, Box, Container, Avatar, IconButton } from '@mui/material'
 import DatosUsuario from '../../Componentes/Perfil/DatosUsuario'
 import Viajes from '../../Componentes/Perfil/Viajes'
 import Calificaciones from '../../Componentes/Perfil/Calificaciones'
-import Amigos from '../../Componentes/Perfil/amigos'
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+
 
 const Perfil = () => {
   const [tabIndex, setTabIndex] = useState(0)
@@ -12,26 +13,51 @@ const Perfil = () => {
     setTabIndex(newIndex)
   }
 
+  const [image, setImage] = useState(
+    "https://scontent.faep8-2.fna.fbcdn.net/v/t1.6435-9/98361598_3114874475297340_7504201618741526528_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=nvrVapNSPAsQ7kNvgHZcNt_&_nc_oc=Adi3tFTGUd6UwwyFEwgBEeolCi0jeYwDrSW5HIyzI5H_p_KX0ngL4faPIxO9OHP1PmA&_nc_zt=23&_nc_ht=scontent.faep8-2.fna&_nc_gid=A-iIqpoy2P5O1pD4e-4-6CF&oh=00_AYH4cftIAEOLWBOHadTo1HHfrvtyijCvW9D_SP5GnOnQkQ&oe=67F87D17"
+  );
+
+  const handleImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setImage(e.target.result);
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
+
   return (
     <Container>
-      {/* Imagen de perfil */}
-      <Box
+         <Box sx={{ position: 'relative', width: 100, height: 100 , ml:'8.5em'}}>
+     
+      <Avatar
+        src={image}
+        alt="perfil"
+        sx={{ width: 100, height: 100 }}
+      />
+      <IconButton
+        component="label"
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          paddingLeft: 2,
-          margin: '0.5rem',
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          '&:hover': {
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+          },
         }}
       >
-        <Avatar
-          src="https://scontent.faep8-2.fna.fbcdn.net/v/t1.6435-9/98361598_3114874475297340_7504201618741526528_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=nvrVapNSPAsQ7kNvgHZcNt_&_nc_oc=Adi3tFTGUd6UwwyFEwgBEeolCi0jeYwDrSW5HIyzI5H_p_KX0ngL4faPIxO9OHP1PmA&_nc_zt=23&_nc_ht=scontent.faep8-2.fna&_nc_gid=A-iIqpoy2P5O1pD4e-4-6CF&oh=00_AYH4cftIAEOLWBOHadTo1HHfrvtyijCvW9D_SP5GnOnQkQ&oe=67F87D17"
-          alt="perfil"
-          sx={{ width: 100, height: 100 }}
+        <CameraAltIcon sx={{ color: 'white', fontSize: 20 }} />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          style={{ display: 'none' }}
         />
-      </Box>
+      </IconButton>
+    </Box>
 
-      {/* Tabs */}
       <Tabs
         value={tabIndex}
         onChange={handleChange}
@@ -43,7 +69,7 @@ const Perfil = () => {
         <Tab label="Calificaciones" />
       </Tabs>
 
-      {/* Contenido */}
+  
       <Box>
         {tabIndex === 0 && <DatosUsuario />}
         {tabIndex === 1 && <Viajes />}
